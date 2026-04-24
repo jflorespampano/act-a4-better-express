0cae4836638fa8ffd88575996652d5d11f3c6c5d
Jesús Alejandro Flores Hernández <jflorespampano@gmail.com>
2026-04-24 07:22:56 -0600

    arreglar la ruta de creacion de base de datos

---
+++ crearDB.js
@@ -0,0 +1,6 @@
+// import { crearDB } from "./src/helpers/crearDB";
+import { crearDB } from "./src/helpers/crearDB.js";
+
+console.log("creando base de datos...")
+crearDB()
+console.log("base de datos creada")
